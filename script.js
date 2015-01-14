53cdffbf187adb8bfc0a8e59ad359a12454575dd
Andrew Kovalenko <andrew@jaybirdgroup.com>
2015-01-14 15:34:16 +0200

    changed concept

--- script.js
+++ script.js
@@ -1,77 +1,24 @@
 var endpoint = 'http://localhost:3000/auth';
 
-var exceptions = [
-    'DB Weekly',
-    'Archives',
-    'Search',
-    'RSS',
-    'Issues',
-    'safe.',
-    'Archives',
-    'Read this issue on the Web' //,
-];
-
-// .container used for HTML5 Weekly, DB Weekly, Node Weekly, JavaScript Weekly
-var links = $('table.container').find('a');
-var linksToSend = [];
-// Web Tools Weekly
-// Web Design Weekly
-
-
-for (var i = 0; i < links.length; i++) {
-    var href = $(links[i]).attr('href');
-    var text = $(links[i]).text();
-
-    // if ($(links[i]).css('font-size') == '17px' || $(links[i]).css('font-size') == '16px') {
-        console.log('Crawling "' + text + '" (' + href + ')');
-        highlightLink($(links[i]));
-        // linksToSend.push(href);
-    // }
-}
-
-var data = {
-    links: linksToSend
-}
-
-console.log(data);
-
-// $.ajax({
-//     type: "POST",
-//     url: endpoint,
-//     data: data,
-//     success: function() {
-//         console.log('Links has been sent!');
-//         window.open(endpoint);
-//     },
-//     error: function(jqXHR, textStatus, err) {
-//         console.log('text status ' + textStatus + ', err ' + err)
-//     },
-//     dataType: 'json'
-// });
-
-$('.crawler-add-link').click(function() {
-    console.log('Crawler add link click...');
-    var el = $(this).parent().parent();
-    var style = css(el);
-    // $("#elementToPutStyleInto").css(style);    
-    console.log(style);
-    return false;
-});
-
-$('.crawler-remove-link').click(function() {
-    console.log('Crawler remove link click...');
-    return false;
-});
-
+addHighlightButton();
 addCrawlButton();
 
-function highlightLink($link) {
-    $link.addClass('crawler-link');
-    addButtons($link);
-}
+function addHighlightButton() {
+    $('body').append('<button class="add-highlight-button">Highilight</button>');
 
-function addButtons($link) {
-    $link.append('<span class="crawler-buttons"><span class="crawler-add-link">+</span><span class="crawler-remove-link">-</span></span>');
+    $('.add-highlight-button').click(function() {
+        
+        $('a').addClass('crawler-link');
+        $('a').click(function() {
+            if ($(this).hasClass('selected'))
+                $(this).removeClass('selected');
+            else
+                $(this).addClass('selected');
+            return false;
+        });
+
+    });
+
 }
 
 function addCrawlButton() {
@@ -79,10 +26,49 @@ function addCrawlButton() {
 
     $('.apply-crawl-button').click(function() {
         $(this).addClass('in-progress');
+        crawl();
     });
 
 }
 
+function crawl() {
+
+    var linksToSend = [];
+
+    var links = $('a.crawler-link.selected');
+
+    for (var i = 0; i < links.length; i++) {
+        var href = $(links[i]).attr('href');
+        var text = $(links[i]).text();
+        console.log('Crawling "' + text + '" (' + href + ')');
+        linksToSend.push(href);
+    }
+
+    var data = {
+        links: linksToSend
+    }
+
+    console.log(data);
+
+    $.ajax({
+        type: "POST",
+        url: endpoint,
+        data: data,
+        success: function() {
+            console.log('Links has been sent!');
+            $('.apply-crawl-button').removeClass('in-progress');
+            window.open(endpoint);
+        },
+        error: function(jqXHR, textStatus, err) {
+            console.log('text status ' + textStatus + ', err ' + err)
+            $('.apply-crawl-button').removeClass('in-progress');
+        },
+        dataType: 'json'
+    });
+
+}
+
+/* Helper functions */
 function css(a) {
     var sheets = document.styleSheets, o = {};
     for (var i in sheets) {
